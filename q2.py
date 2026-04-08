9e1bad90027c3dd66842e9720055f3eaccc24504
nicopetrides123-lgtm <nicopetrides123@gmail.com>
2026-04-08 16:08:22 -0700

    Convert Area and Perimeter to float in output

--- q2.py
+++ q2.py
@@ -2,8 +2,8 @@ length= int(input("enter the length of the rectangle "))
 width= int(input("enter the width of the rectangle "))
 
 Area=length*width
-print(f' the Area is {Area}')
+print(f' the Area is {float(Area)}')
 perimeter=2*(length+width)
-print(f' the Perimeter is {perimeter}')
+print(f' the Perimeter is {float(perimeter)}')
 Diagonal=((length**2)+(width**2))**(1/2)
 print(f" the Diagonal is {Diagonal}")
