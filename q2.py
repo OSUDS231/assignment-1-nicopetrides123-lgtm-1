030d9929e81175417f8418b400d7a2ec776b2158
nicopetrides123-lgtm <nicopetrides123@gmail.com>
2026-04-08 11:39:06 -0700

    fixed issues

--- q2.py
+++ q2.py
@@ -2,8 +2,8 @@ length= int(input("enter the length of the rectangle "))
 width= int(input("enter the width of the rectangle "))
 
 Area=length*width
-print(Area)
+print(f' the Area is {Area}')
 perimeter=2*(length+width)
-print(perimeter)
+print(f' the Perimeter is {perimeter}')
 Diagonal=((length**2)+(width**2))**(1/2)
-print(Diagonal)
+print(f" the Diagonal is {Diagonal}")
