7c56ce6981db4329e8e8540b3eaf382d9a21650a
nicopetrides123-lgtm <nicopetrides123@gmail.com>
2026-04-08 16:45:27 -0700

    Fix driving time formatting in output message

--- q5.py
+++ q5.py
@@ -7,6 +7,6 @@ drive_time=distance/average_speed
 Gallons_needed= distance/fuel_efficiency
 fuel_cost= price_per_gallon*Gallons_needed
 
-print(f"'For a trip of {float(distance)} miles at an average speed of {float(average_speed)} mph, the driving time will be {float(drive_time.1f)} hours.' ")
+print(f"'For a trip of {float(distance)} miles at an average speed of {float(average_speed)} mph, the driving time will be {float(drive_time)} hours.' ")
 print( )
 print(f'Your car will use {float(Gallons_needed)} gallons of gas, and the total fuel cost will be ${float(fuel_cost)} ')
